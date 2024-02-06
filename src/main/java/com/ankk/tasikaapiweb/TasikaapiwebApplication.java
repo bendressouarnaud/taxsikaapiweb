d9c77ef8ad04a75b5ebe207ed6209b664f8cac5a
N-GBANDAMA <arnaud.ngbandama@gcatrans.com>
2024-02-06 14:33:52 +0100

    first commit

---
+++ src/main/java/com/ankk/tasikaapiweb/TasikaapiwebApplication.java
@@ -0,0 +1,13 @@
+package com.ankk.tasikaapiweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasikaapiwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasikaapiwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ankk/tasikaapiweb/TasikaapiwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ankk.tasikaapiweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasikaapiwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
